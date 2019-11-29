790e6cacd68950c1099bc3279a3a0b8306cae6fa
MacCube <maccube@MacCubes-iMac.local>
2019-11-29 19:14:52 +0200

    Index Admin & Index Editor, rout role test

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,15 @@ Route::get('/', 'GuestController@index');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/user', function() {
+    $user = Auth::user()->roles;
+    $role = $user[0];
+    if ($role->role == 'admin'){
+        return view('admin.index');
+    } elseif ($role->role == 'editor') {
+        return view('editor.index');
+    } else {
+        return view('register');
+    }
+});
